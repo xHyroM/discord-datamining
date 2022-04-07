96e546b0671f0c10a1af4179729d37278d638562
xhyrom <generalkubo@gmail.com>
2022-04-07 19:08:20 +0200

    new site

---
+++ packages/site/_config.ts
@@ -0,0 +1,27 @@
+import lume from "https:/deno.land/x/lume@v1.7.2/mod.ts";
+import postcss from "https:/deno.land/x/lume@v1.7.2/plugins/postcss.ts";
+import resolveUrls from "https:/deno.land/x/lume@v1.7.2/plugins/resolve_urls.ts";
+import esbuild from "https:/deno.land/x/lume@v1.7.2/plugins/esbuild.ts";
+
+const site = lume(
+    {
+        src: "./src",
+    }
+);
+
+site
+  .copy("static", ".")
+  .use(resolveUrls())
+  .use(postcss())
+  .use(esbuild({
+    extensions: [".js", ".ts"],
+  }))
+  .scopedUpdates(
+    (path) => path.endsWith(".css"),
+    (path) => path.endsWith(".png") || path.endsWith(".jpg"),
+  )
+  // Filters
+  .filter('split', (str, separator) => str.split(separator))
+  .filter("slice", (arr, length) => arr.slice(0, length));
+
+export default site;
